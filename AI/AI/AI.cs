dc8dbddcdf48afbad46ae65d2ed30f1353e7aa5c
Papucs <papucs@hotmail.com>
2015-05-12 00:13:30 +0200

    ...

--- AI/AI/AI.cs
+++ AI/AI/AI.cs
@@ -1,4 +1,4 @@
-﻿using TicTacToe.Model;
+﻿using Grundy.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,26 +10,26 @@ namespace ai
     public class AI
     {
 
-        public int doMinimax(Logic game)
+        public int doMinimax(IGameModel game)
         {
-            GameTree<State> tree = makeGameTree(game.GetState(), game);
-            return minimax(tree, tree.getDepth(), true);
+            GameTree<IState> tree = makeGameTree(game.GetState(), game);
+            return minimax (tree, tree.getDepth(), true);
         }
 
-        public int doAlphabeta(GameLogic game)
+        public int doAlphabeta(IGameModel game)
         {
-            GameTree<State> tree = makeGameTree(game.GetState(), game);
+            GameTree<IState> tree = makeGameTree(game.GetState(), game);
             return alphaBeta(tree, tree.getDepth(), -1000, 1000, true);
         }
 
-        private GameTree<State> makeGameTree(State actState, GameLogic game)
+        private GameTree<IState> makeGameTree(IState actState, IGameModel game)
         {
 
-            GameTree<State> gt = new GameTree<State>(actState);
+            GameTree<IState> gt = new GameTree<IState>(actState);
 
-            List<State> nextStates = new List<State>();
+            List<IState> nextStates = new List<IState>();
             nextStates.AddRange(game.GetNextStates());
-            foreach (State tmp in nextStates)
+            foreach (IState tmp in nextStates)
             {
                 gt.AddChild(tmp);
             }
@@ -38,18 +38,18 @@ namespace ai
         }
 
 
-        private int minimax(GameTree<State> node, int depth, bool maximizingPlayer)
+        private int minimax(GameTree<IState> node, int depth, bool maximizingPlayer)
         {
 
             if (depth == 0/*|| node is terminal*/)
             {
 
-                return new GameLogic().Evaluate(node.getData());
+                return null;// new IGameModel().Evaluate(node.getData());
             }
             if (maximizingPlayer)
             {
                 int bestValue = -1000;
-                foreach (GameTree<State> item in node)
+                foreach (GameTree<IState> item in node)
                 {
 
                     int val = minimax(item, depth - 1, false);
@@ -61,7 +61,7 @@ namespace ai
             else
             {
                 int bestValue = 1000;
-                foreach (GameTree<State> item in node)
+                foreach (GameTree<IState> item in node)
                 {
                     int val = minimax(item, depth - 1, true);
                     bestValue = Math.Min(bestValue, val);
@@ -71,16 +71,16 @@ namespace ai
             }
         }
 
-        private int alphaBeta(GameTree<State> node, int depth, int alpha, int beta, bool maximizingPlayer)
+        private int alphaBeta(GameTree<IState> node, int depth, int alpha, int beta, bool maximizingPlayer)
         {
             if (depth == 0 /*|| node is terminal*/)
             {
-                return new GameLogic().Evaluate(node.getData());
+                return null;//new Logic().Evaluate(node.getData());
             }
             if (maximizingPlayer)
             {
                 int v = -1000;
-                foreach (GameTree<State> child in node)
+                foreach (GameTree<IState> child in node)
                 {
                     v = Math.Max(v, alphaBeta(child, depth - 1, alpha, beta, false));
                     alpha = Math.Max(alpha, v);
@@ -91,7 +91,7 @@ namespace ai
             else
             {
                 int v = 1000;
-                foreach (GameTree<State> child in node)
+                foreach (GameTree<IState> child in node)
                 {
                     v = Math.Min(v, alphaBeta(child, depth - 1, alpha, beta, true));
                     beta = Math.Min(beta, v);
--- AI/AI/IAI.cs
+++ AI/AI/IAI.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TicTacToe.Model;
+using Grundy.Interface;
 
 namespace ai
 {
     interface IAI
     {
-        int doMinimax(Logic game);
-        int doAlphaBeta(GameTree<State> node, int depth, int alpha, int beta, bool maximizingPlayer);
+        int doMinimax(IGameModel game);
+        int doAlphaBeta(IGameModel game);
     }
 }
 
