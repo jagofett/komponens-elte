869b746f8d1ea2dba6e651524c74cf3acb0c6663
Akos <magas.akos@gmail.com>
2015-05-11 23:41:55 +0200

    added framework reference, star.cs - IGame interface

--- Grundy/Grundy.View/App.xaml.cs
+++ Grundy/Grundy.View/App.xaml.cs
@@ -17,9 +17,7 @@ namespace Grundy.View
     /// </summary>
     public partial class App : Application
     {
-        private GameLogic _model;
-        private MainWindow _view;
-        private GrundyViewModel _viewModel;
+
         public App()
         {
             Startup += new StartupEventHandler(AppStartup);
@@ -27,32 +25,8 @@ namespace Grundy.View
 
         private void AppStartup(object sender, StartupEventArgs startupEventArgs)
         {
-            _model = new GameLogic();
-            _view = new MainWindow();
-            _viewModel = new GrundyViewModel(_model);
-            _view.DataContext = _viewModel;
-            _view.Show();
+
         }
 
-
-        public void StartGame()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void QuitGame()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ICollection GetNextStates()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Evaluate(IState state)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
---
+++ Grundy/Grundy.View/Start.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FrameWork.Model;
+using Grundy.Library.Model;
+using Grundy.Library.ViewModel;
+
+namespace Grundy.View
+{
+    public class Start :IGame
+    {
+        private GameLogic _model;
+        private MainWindow _view;
+        private GrundyViewModel _viewModel;
+        public void newGame()
+        {
+            _model = new GameLogic();
+            _model.GameEnd += ModelGameEnd;
+            _view = new MainWindow();
+            _viewModel = new GrundyViewModel(_model);
+            _view.DataContext = _viewModel;
+            _view.Show();
+        }
+
+        private void ModelGameEnd(object sender, GrundyWinEvenetArgs grundyWinEvenetArgs)
+        {
+            MessageBox.Show("Játék vége, a nyertes: " + grundyWinEvenetArgs.WinnerPlayer.Name, "Játék vége!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+        public void quitGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
