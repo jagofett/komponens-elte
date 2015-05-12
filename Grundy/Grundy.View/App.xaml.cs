ec9f214c892e871753a6428366eddde7fa39c975
Akos <magas.akos@gmail.com>
2015-05-12 20:35:43 +0200

    app.cs readded for  debug logic

--- Grundy/Grundy.View/App.xaml.cs
+++ Grundy/Grundy.View/App.xaml.cs
@@ -17,7 +17,9 @@ namespace Grundy.View
     /// </summary>
     public partial class App : Application
     {
-
+        private GameLogic _model;
+        private MainWindow _view;
+        private GrundyViewModel _viewModel;
         public App()
         {
             Startup += new StartupEventHandler(AppStartup);
@@ -25,8 +27,17 @@ namespace Grundy.View
 
         private void AppStartup(object sender, StartupEventArgs startupEventArgs)
         {
-
+            //only for debug, for release, framework will use the start.cs StartGame method!!!
+            _model = new GameLogic();
+            _model.GameEnd += ModelGameEnd;
+            _view = new MainWindow();
+            _viewModel = new GrundyViewModel(_model);
+            _view.DataContext = _viewModel;
+            _view.Show();
         }
-
+        private void ModelGameEnd(object sender, GrundyWinEvenetArgs grundyWinEvenetArgs)
+        {
+            MessageBox.Show("Játék vége, a nyertes: " + grundyWinEvenetArgs.WinnerPlayer.Name, "Játék vége!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
     }
 }
