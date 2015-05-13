c1ddc13c8195eaefa5dfae1f2bca071b80cee647
Papucs <papucs@hotmail.com>
2015-05-13 09:36:58 +0200

    Update AI.cs

--- AI/AI/AI.cs
+++ AI/AI/AI.cs
@@ -1,4 +1,4 @@
-﻿using Grundy.Interface;
+using FrameWork.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,49 +10,62 @@ namespace ai
     public class AI
     {
 
-        public int doMinimax(IGameModel game)
+        public int doMinimax(IGame game)
         {
-            GameTree<IState> tree = makeGameTree(game.GetState(), game);
-            return minimax (tree, tree.getDepth(), true);
+            GameTree<Object> gt = new GameTree<Object>(game.GetState());
+            GameTree<Object> tree = makeGameTree(gt, game);
+            return minimax(tree, 4, true, game);
         }
 
-        public int doAlphabeta(IGameModel game)
+        public int doAlphabeta(IGame game)
         {
-            GameTree<IState> tree = makeGameTree(game.GetState(), game);
-            return alphaBeta(tree, tree.getDepth(), -1000, 1000, true);
+            GameTree<Object> gt = new GameTree<Object>(game.GetState());
+            GameTree<Object> tree = makeGameTree(gt, game);
+            return alphaBeta(tree,4, -1000, 1000, true, game);
         }
 
-        private GameTree<IState> makeGameTree(IState actState, IGameModel game)
+        private GameTree<Object> makeGameTree(GameTree<Object> gt, IGame game)
         {
 
-            GameTree<IState> gt = new GameTree<IState>(actState);
 
-            List<IState> nextStates = new List<IState>();
-            nextStates.AddRange(game.GetNextStates());
-            foreach (IState tmp in nextStates)
+            List<Object> nextStates = new List<Object>();
+            nextStates.AddRange(game.GetNextStates(gt.getData()));
+            foreach (Object tmp in nextStates)
             {
                 gt.AddChild(tmp);
             }
+
+
+            foreach (GameTree<Object> x in gt)
+            {
+                List<Object> tmp = game.GetNextStates(x.getData());
+                x.AddChildren(tmp);
+                foreach(GameTree<Object> c in x){
+                    tmp = game.GetNextStates(c.getData());
+                    c.AddChildren(tmp);
+                }
+            }
+
 
             return gt;
         }
 
 
-        private int minimax(GameTree<IState> node, int depth, bool maximizingPlayer)
+        private int minimax(GameTree<Object> node, int depth, bool maximizingPlayer, IGame game)
         {
 
             if (depth == 0/*|| node is terminal*/)
             {
 
-                return null;// new IGameModel().Evaluate(node.getData());
+                return game.Evaluate(node.getData());
             }
             if (maximizingPlayer)
             {
                 int bestValue = -1000;
-                foreach (GameTree<IState> item in node)
+                foreach (GameTree<Object> item in node)
                 {
 
-                    int val = minimax(item, depth - 1, false);
+                    int val = minimax(item, depth - 1, false, game);
                     bestValue = Math.Max(bestValue, val);
 
                 }
@@ -61,9 +74,9 @@ namespace ai
             else
             {
                 int bestValue = 1000;
-                foreach (GameTree<IState> item in node)
+                foreach (GameTree<Object> item in node)
                 {
-                    int val = minimax(item, depth - 1, true);
+                    int val = minimax(item, depth - 1, true, game);
                     bestValue = Math.Min(bestValue, val);
 
                 }
@@ -71,18 +84,18 @@ namespace ai
             }
         }
 
-        private int alphaBeta(GameTree<IState> node, int depth, int alpha, int beta, bool maximizingPlayer)
+        private int alphaBeta(GameTree<Object> node, int depth, int alpha, int beta, bool maximizingPlayer, IGame game)
         {
             if (depth == 0 /*|| node is terminal*/)
             {
-                return null;//new Logic().Evaluate(node.getData());
+                return game.Evaluate(node.getData());
             }
             if (maximizingPlayer)
             {
                 int v = -1000;
-                foreach (GameTree<IState> child in node)
+                foreach (GameTree<Object> child in node)
                 {
-                    v = Math.Max(v, alphaBeta(child, depth - 1, alpha, beta, false));
+                    v = Math.Max(v, alphaBeta(child, depth - 1, alpha, beta, false, game));
                     alpha = Math.Max(alpha, v);
                     if (beta < alpha) break;
                 }
@@ -91,9 +104,9 @@ namespace ai
             else
             {
                 int v = 1000;
-                foreach (GameTree<IState> child in node)
+                foreach (GameTree<Object> child in node)
                 {
-                    v = Math.Min(v, alphaBeta(child, depth - 1, alpha, beta, true));
+                    v = Math.Min(v, alphaBeta(child, depth - 1, alpha, beta, true, game));
                     beta = Math.Min(beta, v);
                     if (beta <= alpha) break;
                 }
