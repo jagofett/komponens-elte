0dd503f831a590270be6a81b269d7da16aa38be1
Papucs <papucs@hotmail.com>
2015-05-13 09:37:53 +0200

    Update IAI.cs

--- AI/AI/IAI.cs
+++ AI/AI/IAI.cs
@@ -1,16 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Grundy.Interface;
-
+using FrameWork.Model;
 namespace ai
 {
     interface IAI
     {
-        int doMinimax(IGameModel game);
-        int doAlphaBeta(IGameModel game);
+        int doMinimax(IGame game);
+        int doAlphaBeta(IGame game);
     }
 }
 
