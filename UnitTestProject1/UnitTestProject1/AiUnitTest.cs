04242ab5f15492df07e5ca847a16bc8c37f5292c
Papucs <papucs@hotmail.com>
2015-05-13 19:25:02 +0200

    v
    nem tudom mi történik

---
+++ UnitTestProject1/UnitTestProject1/AiUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interfaces;
+using TicTacToeLibrary;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AiUnitTest
+    {
+        [TestMethod]
+        public void Minimax_Test(IGame game)
+        {
+            
+        }
+
+        [TestMethod]
+        public void AlphaBeta_Test(IGame game)
+        {
+
+        }
+    }
+}
