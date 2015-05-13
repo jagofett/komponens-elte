46d4bcace15482116d64a81cbe73c143596f942a
szalontaizoli <szalontai.zoli@hotmail.com>
2015-05-13 23:51:13 +0200

    zoli a király

---
+++ FrameWork/FrameWork/Model/GameMock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FrameWork.Model
+{
+    class GameMock : IGame
+    {
+        Window window;
+
+        public void newGame()
+        {
+            window = new Window();
+            window.Show();
+        }
+
+        public void quitGame()
+        {
+            window.Close();
+        }
+    }
+}
